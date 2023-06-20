bb4eacc3fa1af728977e423387e4b0fb4b8ca6a8
Luciano Repetti <lucianorepetti2020@gmail.com>
2023-06-20 15:49:14 -0300

    backend url and notification settings

---
+++ src/config/backend.js
@@ -0,0 +1,7 @@
+// Production or developmente variable
+const PROD_URL = "";
+const DEV_URL = "http://localhost:3001";
+
+const API_URL = DEV_URL;
+
+export default API_URL;
---
+++ src/config/notifications.js
@@ -0,0 +1,38 @@
+export const DEFAULT_CONFIG = {
+  position: "bottom-left",
+  hideProgressBar: true,
+  closeButton: false,
+  closeOnClick: false,
+  style: {
+    boxShadow: "none",
+    background: "black",
+    color: "white",
+    fontSize: "0.8rem",
+  },
+};
+
+export const ERROR_CONFIG = {
+  position: "bottom-left",
+  hideProgressBar: true,
+  closeButton: false,
+  closeOnClick: false,
+  style: {
+    boxShadow: "none",
+    background: "#e23341",
+    color: "white",
+    fontSize: "0.8rem",
+  },
+};
+
+export const SUCCESS_CONFIG = {
+  position: "bottom-left",
+  hideProgressBar: true,
+  closeButton: false,
+  closeOnClick: false,
+  style: {
+    boxShadow: "none",
+    background: "#25CC57",
+    color: "#000",
+    fontSize: "0.8rem",
+  },
+};
